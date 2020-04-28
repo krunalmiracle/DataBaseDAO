01cec6ea314157668cecabb94cf22787bb3886a1
KRUNAL <krunalmiracle@gmail.com>
2020-04-28 20:29:49 +0100

    Merged Atenea DAO with BDD-DAO Merged the Atenea project with BDD-DAO.

---
+++ src/main/java/edu/upc/eetac/dsa/IEmployeeDAO.java
@@ -0,0 +1,15 @@
+package edu.upc.eetac.dsa;
+
+import edu.upc.eetac.dsa.models.Employee;
+
+import java.util.List;
+
+public interface IEmployeeDAO {
+
+    int addEmployee(String name, String surname, double salary);
+    Employee getEmployee(int employeeID);
+    void updateEmployee(int employeeID, String name, String surname, double salary);
+    void deleteEmployee(int employeeID);
+    List<Employee> getEmployees();
+    List <Employee> getEmployeeByDept(int deptId);
+}
---
+++ src/main/java/edu/upc/eetac/dsa/models/Deparment.java
@@ -0,0 +1,11 @@
+package edu.upc.eetac.dsa.models;
+
+public class Deparment {
+    private final String name;
+    private final String description;
+
+    public Deparment(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
